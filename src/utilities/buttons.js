c6244a2b55b529fcc89fe821a3a81a912b34d032
Vinoth Kannan <vinothkannan@vinkas.com>
2024-11-15 21:58:43 +0530

    update readme file with available utilities

--- src/utilities/buttons.js
+++ src/utilities/buttons.js
@@ -2,10 +2,4 @@ module.exports = {
   '.btn': {
     '@apply inline-flex items-center rounded py-2 px-4 text-sm font-medium leading-4 transition duration-150 ease-in-out': {},
   },
-  '.btn-primary': {
-    '@apply bg-primary text-primary-contrast': {},
-  },
-  '.btn-secondary': {
-    '@apply bg-secondary text-secondary-contrast': {},
-  },
 }
